4410783967e0f5ed32e1d287befffd0a0b9cfe67
memd24 <memd24@gmail.com>
2017-08-09 18:15:17 -0500

    index con header

---
+++ CaseAndMe/Models/ManageViewModels/LoadCategoriasViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CaseAndMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CaseAndMe.ViewModels.ManageViewModels
+{
+    public class LoadCategoriasViewModel
+    {
+        //public string idCategoria { get; set; }
+        //public List<Categoria> CategoriaList { get; set; }
+    }
+}
