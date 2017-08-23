784f6a60f923c70a34a1fc82b32076ccf97c3de0
memd24 <memd24@gmail.com>
2017-08-23 00:07:56 -0500

    se modifican html de login, y demas

--- CaseAndMe/Models/AccountViewModels/LoginViewModel.cs
+++ CaseAndMe/Models/AccountViewModels/LoginViewModel.cs
@@ -16,7 +16,7 @@ namespace CaseAndMe.Models.AccountViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Recordar mis Datos")]
         public bool RememberMe { get; set; }
     }
 }
