84c554204cf3142d3d76e5f464d12fde3a3eadfa
Gerardo Sánchez Hernández <yerald231ger@live.com>
2018-08-30 16:40:39 -0500

    metodo de actualizar

--- CaseAndMeWeb/Services/Repository/IRepository.cs
+++ CaseAndMeWeb/Services/Repository/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CaseAndMeWeb.Models;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 
 namespace CaseAndMeWeb.Services.Repository
@@ -7,11 +8,14 @@ namespace CaseAndMeWeb.Services.Repository
     public interface IRepository<TEntity, TKey> where TEntity : Base<TKey>
     {
         ICollection<TEntity> GetAll();
+        void Update(TEntity entity);
+        int UpdateNow(TEntity entity);
     }
 
     public class Repository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntity : Base<TKey>
     {
         protected ApplicationDbContext _context { get; set; }
+        protected DbSet<TEntity> _dbSet { get; set; }
 
         public Repository(ApplicationDbContext context)
         {
@@ -22,5 +26,19 @@ namespace CaseAndMeWeb.Services.Repository
         {
             return _context.Set<TEntity>().ToList();
         }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public int UpdateNow(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            return _context.SaveChanges();
+        }
+
     }
 }
