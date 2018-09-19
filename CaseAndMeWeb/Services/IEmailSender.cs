953cb30fd4a304982f2bd114a6c8dfe4281a3546
Gerardo Sánchez Hernández <yerald231ger@live.com>
2018-09-19 10:08:39 -0500

    envio de email

--- CaseAndMeWeb/Services/IEmailSender.cs
+++ CaseAndMeWeb/Services/IEmailSender.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace CaseAndMeWeb.Services
@@ -10,12 +12,32 @@ namespace CaseAndMeWeb.Services
         Task SendEmailAsync(string email, string subject, string message);
     }
 
-    public class EmailSender : IEmailSender
-    {
-        public Task SendEmailAsync(string email, string subject, string message)
-        {
-            return null;
-        }
-    }
+    //public class EmailSender : IEmailSender
+    //{
+    //    public Task SendEmailAsync(string email, string subject, string message)
+    //    {
+
+    //        var _fromAddress = new MailAddress("angarerp@gmail.com", "Angar Account Services");
+    //        var _toAddress = new MailAddress(message.Destination);
+
+    //        var smtp = new SmtpClient
+    //        {
+    //            Host = "smtp.gmail.com",
+    //            Port = 587,
+    //            EnableSsl = true,
+    //            DeliveryMethod = SmtpDeliveryMethod.Network,
+    //            UseDefaultCredentials = false,
+    //            Credentials = new NetworkCredential(_fromAddress.Address, "!@Ger231")
+    //        };
+
+    //        var _mail = new MailMessage(_fromAddress, _toAddress);
+    //        _mail.Body = message.Body;
+    //        _mail.Subject = message.Subject;
+    //        _mail.IsBodyHtml = true;
+
+    //        return smtp.SendMailAsync(_mail);
+
+    //    }
+    //}
 }
 
