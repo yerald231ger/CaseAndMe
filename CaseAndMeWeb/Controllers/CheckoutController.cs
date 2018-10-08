61810b759a8de74d38c32ff43c7cd6e6900fe28d
Martin Eduardo Martinez Dimas <memd24@gmail.com>
2018-10-08 11:11:41 -0500

    Se carga Deliver

--- CaseAndMeWeb/Controllers/CheckoutController.cs
+++ CaseAndMeWeb/Controllers/CheckoutController.cs
@@ -71,36 +71,24 @@ namespace CaseAndMeWeb.Controllers
         // GET: Checkout/Deliver
         public ActionResult Deliver()
         {
+           
             var userId = User.Identity.GetUserId();
             var usuario = context.Users.Where(x=> x.Id == userId).FirstOrDefault();
             if(usuario != null)
             {
+                
+
+
                 int estadoDefault = usuario.IdEstado;
                 var paisDefault = context.Estados.Where(x => x.Id == estadoDefault).FirstOrDefault();
 
                 var paisesList = context.Paises.ToList();
                 var estadosList = context.Estados.Where(x=> x.IdPais == paisDefault.IdPais).ToList();
 
-                var listItemsPaises = new List<SelectListItem>();
-                var listItemsEstados = new List<SelectListItem>();
+               
 
-                foreach (var p in paisesList)
-                    listItemsPaises.Add(new SelectListItem
-                    {
-                        Text = p.Nombre,
-                        Value = p.Id.ToString(),
-                        Selected = p.Id == paisDefault.IdPais
-                    });
-
-                foreach (var p in estadosList)
-                    listItemsEstados.Add(new SelectListItem
-                    {
-                        Text = p.Nombre,
-                        Value = p.Id.ToString()
-                    });
-
-                ViewBag.Paises = new SelectList(paisesList, "Id", "Nombre", paisDefault);
-                ViewBag.Estados = listItemsEstados;
+                ViewBag.Paises = new SelectList(paisesList, "Id", "Nombre", paisDefault.IdPais);
+                ViewBag.Estados = new SelectList(estadosList, "Id", "Nombre", estadoDefault); ;
 
                 usuario.PrimerApellido = usuario.PrimerApellido + " " + usuario.SegundoApellido;
 
