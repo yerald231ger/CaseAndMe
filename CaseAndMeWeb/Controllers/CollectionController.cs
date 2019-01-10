4345ad18cc805c171b237b116ce72c91a33b9c86
Martin Eduardo Martinez Dimas <memd24@gmail.com>
2019-01-09 22:01:44 -0600

    Gera! Se crea Vista Coleccion Martin! Se crea Vista Personalizador

---
+++ CaseAndMeWeb/Controllers/CollectionController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaseAndMeWeb.Controllers
+{
+    public class CollectionController : Controller
+    {
+        // GET: Collection
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Collection/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Collection/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Collection/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Collection/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Collection/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Collection/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Collection/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CaseAndMeWeb/Controllers/PersonalizadorController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaseAndMeWeb.Controllers
+{
+    public class PersonalizadorController : Controller
+    {
+        // GET: Personalizador
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Personalizador/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Personalizador/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personalizador/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Personalizador/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Personalizador/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Personalizador/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Personalizador/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
