2a64e67f67220dc87fbf51181680bffd3362e44a
Martin Eduardo Martinez Dimas <memd24@gmail.com>
2019-04-20 17:29:55 -0500

    Se crea Catalogo Orden de Venta Se descarga imagenes de Orden de Venta Detalle

---
+++ CaseAndMeWeb/Controllers/OrdenVentaController.cs
@@ -0,0 +1,113 @@
+﻿using CaseAndMeWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaseAndMeWeb.Controllers
+{
+    public class OrdenVentaController : Controller
+    {
+        public ApplicationDbContext context { get; set; }
+
+        public OrdenVentaController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: OrdenVenta
+        public ActionResult Index()
+        {
+            var MetodosEnvio = context.MetodosEnvios.ToList();
+            var MetodosPago = context.MetodosPagos.ToList();
+            var OrdenesVenta = context.OrdenesVentas.OrderByDescending(x => x.FechaMod).ToList();
+            ViewBag.OrdenesVenta = (from o in OrdenesVenta
+                                    join mp in MetodosPago on o.IdMetodoPago equals mp.Id
+                                    join me in MetodosEnvio on o.IdMetodoEnvio equals me.Id
+                                    select new
+                                    {
+                                        o.Id,
+                                        o.Folio,
+                                        NombreCompleto = o.Nombre + " " + o.Apellido,
+                                        o.Email,
+                                        MetodoEnvio = me.Nombre,
+                                        MetodoPago = mp.Nombre,
+                                        o.FechaMod
+                                    }).ToList();
+            return View();
+        }
+
+        // GET: OrdenVenta/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OrdenVenta/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrdenVenta/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrdenVenta/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OrdenVenta/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrdenVenta/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OrdenVenta/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CaseAndMeWeb/Controllers/OrdenVentaDetalleController.cs
@@ -0,0 +1,125 @@
+﻿using CaseAndMeWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaseAndMeWeb.Controllers
+{
+    public class OrdenVentaDetalleController : Controller
+    {
+        public ApplicationDbContext context { get; set; }
+
+        public OrdenVentaDetalleController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        // GET: OrdenVentaDetalle
+        public ActionResult Index(int? id)
+        {
+            if(id != null && id != 0)
+            {
+                var OrdenVenta = context.OrdenesVentas.Where(x => x.Id == id).FirstOrDefault();
+                if (OrdenVenta != null)
+                {
+                    ViewBag.OrdenVenta = OrdenVenta;
+                    var productos = context.Productos.ToList();
+                    var dispositivos = context.Dispositivo.ToList();
+                    var materiales = context.Material.ToList();
+
+                    var OrdenesVentaDetalle = context.OrdenesVentasDetalle.Where(x => x.IdOrdenVenta == OrdenVenta.Id).ToList();
+                    ViewBag.OrdenesVentaDetalle = (from ovd in OrdenesVentaDetalle
+                                                   join p in productos on ovd.IdProducto equals p.Id
+                                                   join d in dispositivos on ovd.IdDipositivo equals d.Id
+                                                   join m in materiales on ovd.IdMaterial equals m.Id
+                                                   select new
+                                                   {
+                                                       NombreProducto = p.Nombre,
+                                                       NombreDispositivo = d.Nombre,
+                                                       NombreMaterial = m.Nombre,
+                                                       ovd.Cantidad,
+                                                       ovd.Precio,
+                                                       ovd.IdProducto,
+                                                       ovd.Imagen
+                                                   }
+                        ).ToList();
+                }
+            }
+            
+            return View();
+        }
+
+        // GET: OrdenVentaDetalle/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OrdenVentaDetalle/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrdenVentaDetalle/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrdenVentaDetalle/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OrdenVentaDetalle/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrdenVentaDetalle/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OrdenVentaDetalle/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
